059e5fa5d1b23bb0c2175ede22c6a1c4684ce66b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-15 10:06:55 +0400

    feat: install rust analyzer features

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,7 +18,36 @@ return {
           },
         },
         eslint = {},
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              diagnostics = {
+                disabled = { "inactive-code" }, -- ← disables the warning
+              },
+              -- Optional: always enable certain features (like "ssr")
+              cargo = {
+                features = { "all" },
+              },
+              procMacro = {
+                ignored = {
+                  leptos_macro = {
+                    -- optional: --
+                    -- "component",
+                    "server",
+                  },
+                },
+              },
+            },
+          },
+        },
       },
     },
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
   },
 }
