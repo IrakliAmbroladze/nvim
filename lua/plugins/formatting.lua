4bc612f0571e134e60ca54057790c0c1576a505d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-01 19:19:09 +0400

    feat(formatting): added leptosfmt formatter

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -11,6 +11,15 @@ return {
         html = { "prettier" },
         css = { "prettier" },
         markdown = { "prettier" },
+        -- if leptosfmt overrides rustfmt
+        -- I will need this version: rust = { "leptosfmt", "rustfmt" }
+        rust = { "leptosfmt" },
+      },
+      formatters = {
+        leptosfmt = {
+          command = "leptosfmt",
+          args = { "--stdin" },
+        },
       },
     },
   },
